f6f7b96bfe4474096cd54993e1233118a90ddc2e
CahitAkcay <CahitErdemAkcay@gmail.com>
2022-04-05 12:38:26 +0100

    API Update class

--- src/test/java/apitests/HamCrestMatchersApiTest.java
+++ src/test/java/apitests/HamCrestMatchersApiTest.java
@@ -8,6 +8,18 @@ import static org.hamcrest.Matchers.*;
 
 
 public class HamCrestMatchersApiTest {
+    /*
+     given accept type is Json
+     And path param id is 15
+     When user sends a get request to spartans/{id}
+     Then status code is 200
+     And content type is Json
+     And json data has following
+         "id": 15,
+         "name": "Meta",
+         "gender": "Female",
+         "phone": 1938695106
+      */
      /*
       given accept type is Json
       And path param id is 15
@@ -21,25 +33,48 @@ public class HamCrestMatchersApiTest {
           "phone": 1938695106
        */
     @Test
-    public void OneSpartanWithHamcrest(){
+    public void OneSpartanWithHamcrest() {
         given().accept(ContentType.JSON)
-                .and().pathParam("id",15)
-                .and().get("http://100.26.60.119:8000/api/spartans/{id}")
+                .and().pathParam("id", 15).
+                when().get("http://54.91.210.3:8000/api/spartans/{id}")
                 .then().statusCode(200)
-                .and().assertThat().contentType("application/json")
-                .and().assertThat().body("id",equalTo(15),
-                        "name",equalTo("Meta"),
-                        "gender",equalTo("Female"),
-                        "phone",equalTo(1938695106));
+                .and().assertThat().contentType(equalTo("application/json"))
+                .and().assertThat().body("id", equalTo(15),
+                        "name", equalTo("Meta"),
+                        "gender", equalTo("Female"),
+                        "phone", equalTo(1938695106));
+
 
     }
+
     @Test
-    public void teacherData(){
+    public void teacherData() {
         given().accept(ContentType.JSON)
-                .and().pathParam("name","Computer")
+                .and().pathParam("id", 8261)
+                .when().log().all().get("http://api.cybertektraining.com/teacher/{id}")
+                .then().assertThat().statusCode(200)
+                .and().contentType(equalTo("application/json;charset=UTF-8"))
+                .and().header("Content-Length", equalTo("240"))
+                .and().header("Connection", equalTo("Keep-Alive"))
+                .and().header("Date", notNullValue())
+                .and().assertThat().body("teachers.firstName[0]", equalTo("James"),
+                        "teachers.lastName[0]", equalTo("Bond"),
+                        "teachers.gender[0]", equalTo("Male"))
+                .log().all()
+        ;
+
+    }
+
+    @Test
+    public void teachersWithDepartments() {
+
+        given().accept(ContentType.JSON)
+                .and().pathParam("name", "Computer")
                 .when().log().all().get("http://api.cybertektraining.com/teacher/department/{name}")
                 .then().statusCode(200).and()
                 .contentType(equalTo("application/json;charset=UTF-8")).and()
-                .body("teachers.firstName",hasItems("Alexander","Marteen"));
+                .body("teachers.firstName", hasItems("Alexander", "Marteen"));
+
+
     }
 }
