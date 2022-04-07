7667024820d1428a65d1d8fb342c16f55911fe42
CahitAkcay <CahitErdemAkcay@gmail.com>
2022-04-07 13:24:05 +0100

    API

---
+++ src/test/java/Day6_POJO/Pojo_deserialize.java
@@ -0,0 +1,21 @@
+package Day6_POJO;
+
+import com.google.gson.Gson;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.testng.Assert.assertEquals;
+
+public class Pojo_deserialize {
+
+    @Test
+    public void oneSpartanPojo() {
+
+    }
+}
---
+++ src/test/java/Day6_POJO/Spartan.java
@@ -0,0 +1,13 @@
+package Day6_POJO;
+
+/*
+    {
+        "id": 15,
+        "name": "Meta",
+        "gender": "Female",
+        "phone": 1938695106
+    }
+ */
+
+public class Spartan {
+}
