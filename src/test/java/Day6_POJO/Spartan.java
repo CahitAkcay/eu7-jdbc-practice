597365d5d7e57f1e9f3294de32a7712df9c0f24c
CahitAkcay <CahitErdemAkcay@gmail.com>
2022-04-07 13:31:51 +0100

    Accepting the values

--- src/test/java/Day6_POJO/Spartan.java
+++ src/test/java/Day6_POJO/Spartan.java
@@ -1,5 +1,10 @@
 package Day6_POJO;
 
+//we create this by using Generate
+//than choose Getter and Setter
+//also Constructor and now everything is ready
+//it's ready to accepting the values
+
 /*
     {
         "id": 15,
@@ -10,4 +15,52 @@ package Day6_POJO;
  */
 
 public class Spartan {
+
+    private int id;
+    private String name;
+    private String gender;
+    private long phone;
+
+    public Spartan(int id, String name, String gender, long phone) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.phone = phone;
+    }
+
+    public Spartan() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
 }
