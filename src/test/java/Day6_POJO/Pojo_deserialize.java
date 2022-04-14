25b08d52d9390573d0a6483e0bb8d35f6ddae6b5
CahitAkcay <CahitErdemAkcay@gmail.com>
2022-04-14 11:32:58 +0100

    Json to PoJo

--- src/test/java/Day6_POJO/Pojo_deserialize.java
+++ src/test/java/Day6_POJO/Pojo_deserialize.java
@@ -42,9 +42,42 @@ public class Pojo_deserialize {
          assertEquals(response.statusCode(),200);
 
         //JSON to POJO(regions class)
+        Regions regions = response.body().as(Regions.class);
 
+        System.out.println("regions.getHasMore() = " + regions.getHasMore());
+        System.out.println("regions.getCount() = " + regions.getCount());
 
+        System.out.println(regions.getItems().get(0).getRegionName());
 
+        List<Item> items = regions.getItems();
+
+        System.out.println(items.get(1).getRegionId());
+    }
+    @Test
+    public void gson_example(){
+
+        Gson gson = new Gson();
+
+        //JSON to Java collections or Pojo --> De-serialization
+
+        String myJsonData = "{\n" +
+                "    \"id\": 15,\n" +
+                "    \"name\": \"Meta\",\n" +
+                "    \"gender\": \"Female\",\n" +
+                "    \"phone\": 1938695106\n" +
+                "}";
+        Map map = gson.fromJson(myJsonData, Map.class);
+        System.out.println(map);
+
+        Spartan spartan15 = gson.fromJson(myJsonData,Spartan.class);
+        System.out.println(spartan15);
+
+        //-------SERIALIZATION---------
+        //JAVA collection or POJO to JSON
+        Spartan spartanEu = new Spartan(200,"Mike","Male",123123123);
+
+        String jsonSpartanEu = gson.toJson(spartanEu);
+        System.out.println(jsonSpartanEu);
 
     }
 }
